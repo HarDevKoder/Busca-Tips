5f8b02cdf399179c8f544f74c7d1383a9471e08b
HarDevKoder <haroldvaldes@yahoo.com>
2023-11-29 17:46:45 -0500

    Agrego links a notion y predetermino select(Seleccionar) al regresar de pagina linkeada

--- js/libreria.js
+++ js/libreria.js
@@ -6,30 +6,36 @@
 export const referenciarElementosDom = () => {
   const selectorCategorias = document.getElementById("selectorCategorias");
   const contenedorResultados = document.getElementById("contenedorResultados");
-  const listaResultados = document.getElementById('listaResultados');
+  const listaResultados = document.getElementById("listaResultados");
 };
 
 // -----------------------------------------------------------------------
 // Mensaje de confirmación de Categoria seleccionada
 // -----------------------------------------------------------------------
 export const cargarArchivoCategoria = () => {
-    let archivoSeleccionado = selectorCategorias.value;
-    return archivoSeleccionado;
+  let archivoSeleccionado = selectorCategorias.value;
+  return archivoSeleccionado;
 };
 
 // -----------------------------------------------------------------------
 // Extraccion de los valores completos del JSON selecionado
 // -----------------------------------------------------------------------
-export const extraerDatoCompletoJson = (url, callback) =>{
+export const extraerDatoCompletoJson = (url, callback) => {
   fetch(url)
-    .then(res => res.json())
-    .then((salida)=>{
+    .then((res) => res.json())
+    .then((salida) => {
       callback(salida);
-    })
-}
+    });
+};
 
 // -----------------------------------------------------------------------
-// 
+// Determino el origen de la carga de la pagina
 // -----------------------------------------------------------------------
-
-
+export const origenCargaPagina = () => {
+  var entries = performance.getEntriesByType("navigation");
+  if (entries[0].type === "back_forward") {
+    // La página fue navegada a través del botón de adelante o atrás
+    // Refrescar la página
+    document.location.reload();
+  }
+};
--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
+
 // Importacion de Funciones;
 import {
   referenciarElementosDom,
   cargarArchivoCategoria,
   extraerDatoCompletoJson,
+  origenCargaPagina
 } from "./libreria.js";
+
+// Origen de la carga de la pagina (inicial o retroceso)
+origenCargaPagina();
 
 // Variables Globales
 //Archivo inicial del select
-let archivoSeleccionado = "json/git.json";
+let archivoSeleccionado = "json/Seleccionar.json";
 
 // ----------------------------------------------------------------------
 // PROGRAMA PRINCIPAL
 // ----------------------------------------------------------------------
-listaResultados.style.display = "none";
 
 // Referencio Elementos del DOM
 referenciarElementosDom();
 
 // Selecciono Archivo para extraer Tips
 selectorCategorias.addEventListener("change", () => {
-  listaResultados.innerHTML = " ";
-  listaResultados.style.display = "block";
+  listaResultados.innerText = " ";
   archivoSeleccionado = cargarArchivoCategoria();
-  if (archivoSeleccionado === "json/Seleccionar.json"){
-    listaResultados.style.display = "none";;
-  }else{
+  if (archivoSeleccionado === "json/Seleccionar.json") {
+    listaResultados.style.display = "none";
+  } else {
     extraerDatoCompletoJson(archivoSeleccionado, (salida) => {
+      listaResultados.style.display = "block";
       salida.data.forEach((item) => {
         let li = document.createElement("li");
-        li.innerHTML = item.titulo;
+        let a = document.createElement("a");
+        a.textContent = item.titulo;
+        a.href = item.link;
+        a.style.textDecoration = 'none';
+        a.style.color = 'Black';
+        a.style.fontWeight = 'Black';
+        li.appendChild(a);
         listaResultados.appendChild(li);
       });
     });
