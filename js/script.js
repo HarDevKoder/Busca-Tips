28926953b43cd9af4810a6d9e2622cc9b1f81762
HarDevKoder <haroldvaldes@yahoo.com>
2024-01-10 21:44:50 -0500

    Creacion del Service worker

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 // --------------------------------------------------------------
 // Importacion de Funciones
 // --------------------------------------------------------------
-import {
-  referenciarElementosDom,
-  desplegarTips,
-} from "./libreria.js";
+import { referenciarElementosDom, desplegarTips } from "./libreria.js";
 
 // --------------------------------------------------------------
 // Origen de la carga de la pagina (inicial o retroceso)
@@ -19,7 +16,20 @@ let archivoSeleccionado = "json/Seleccionar.json";
 // --------------------------------------------------------------
 // PROGRAMA PRINCIPAL
 // --------------------------------------------------------------
+
+// Compruebo existencia de service worker (PWA)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+  .register("./sw.js")
+  .then((reg) => console.log("Registro de SW exitoso", reg))
+  .catch((err) => console.warn("Error al tratar de registrar el sw", err));
+}
+
+
+// Referencio elementos del DOM
 referenciarElementosDom();
+
+// Asigno logo a mostrar al cargar la pagina
 contenedorLogoTech.src = "imagenes/programacion.svg";
 
 // Selecciono Archivo para extraer Tips y manipulo sus datos
@@ -27,4 +37,3 @@ selectorCategorias.addEventListener("change", () => {
   archivoSeleccionado = selectorCategorias.value;
   desplegarTips(archivoSeleccionado);
 });
-
